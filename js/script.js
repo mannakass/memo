0450f1f00647a9c4c3f8d9c3d74b06312c277346
Hanna Kass <hanna.kass7@gmail.com>
2025-12-15 12:30:39 +0200

    Created a list of characters

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
-const img = document.querySelector("img");
-const p = document.querySelector("p");
+const body = document.querySelector("body");
 
-fetch("https://api.disneyapi.dev/character")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    const randomNumber = Math.floor(Math.random() * response.data.length);
+/* creating memo gameboard*/
+function gameboard() {
+  for (let i = 0; i < 16; i++) {
+    /* getting info from API */
+    fetch("https://api.disneyapi.dev/character")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        /* creating new HTML elements */
+        const newDiv = document.createElement("div");
+        const image = document.createElement("img");
+        const name = document.createElement("p");
 
-    img.src = response.data[randomNumber].imageUrl;
-    p.textContent = response.data[randomNumber].name;
-  });
+        /* applying API information to the elements I created*/
+        const randomNumber = Math.floor(Math.random() * response.data.length);
+        image.src = response.data[randomNumber].imageUrl;
+        name.textContent = response.data[randomNumber].name;
+
+        /* putting the information into the HTML */
+        newDiv.appendChild(image);
+        newDiv.appendChild(name);
+        body.appendChild(newDiv);
+      });
+  }
+}
+
+/* calling the function */
+gameboard();
