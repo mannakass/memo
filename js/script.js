d04227c0429bca6e194ee1759ef84e555d5a671e
Hanna Kass <hanna.kass7@gmail.com>
2025-12-15 13:03:25 +0200

    created duplicates if pictures

--- js/script.js
+++ js/script.js
@@ -2,27 +2,30 @@ const memoboard = document.querySelector(".memo-gameboard");
 
 /* creating memo gameboard*/
 function gameboard() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 8; i++) {
     /* getting info from API */
     fetch("https://api.disneyapi.dev/character")
       .then(function (response) {
         return response.json();
       })
       .then(function (response) {
-        /* creating new HTML elements */
-        const newDiv = document.createElement("div");
-        const image = document.createElement("img");
-        const name = document.createElement("p");
-
-        /* applying API information to the elements I created*/
         const randomNumber = Math.floor(Math.random() * response.data.length);
-        image.src = response.data[randomNumber].imageUrl;
-        name.textContent = response.data[randomNumber].name;
 
-        /* putting the information into the HTML */
-        newDiv.appendChild(image);
-        newDiv.appendChild(name);
-        memoboard.appendChild(newDiv);
+        /* creating new HTML elements */
+        for (let j = 0; j < 2; j++) {
+          const newDiv = document.createElement("div");
+          const image = document.createElement("img");
+          const name = document.createElement("p");
+
+          /* applying API information to the elements I created*/
+          image.src = response.data[randomNumber].imageUrl;
+          name.textContent = response.data[randomNumber].name;
+
+          /* putting the information into the HTML */
+          newDiv.appendChild(image);
+          newDiv.appendChild(name);
+          memoboard.appendChild(newDiv);
+        }
       });
   }
 }
