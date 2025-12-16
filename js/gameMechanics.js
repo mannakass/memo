275347ccb3b942640de7d076f0e0c34250619455
Hanna Kass <hanna.kass7@gmail.com>
2025-12-16 14:04:04 +0200

    changes page after completing

--- js/gameMechanics.js
+++ js/gameMechanics.js
@@ -1,3 +1,5 @@
+//const memoboard = document.querySelector(".memo-gameboard");
+const memoryboard = document.querySelector(".memo-gameboard");
 let cardOneID;
 let cardTwoID;
 let cardOneName;
@@ -5,6 +7,9 @@ let cardTwoName;
 let clickCount = 0;
 
 function toggleDat(givenID) {
+  console.log(document.querySelector(".memo-gameboard"));
+  console.log(document.querySelector(".memo-gameboard").children);
+
   let elementID = document.getElementById(givenID);
   let elementText = elementID.textContent;
 
@@ -28,6 +33,19 @@ function checkHowManyClicked(name, specialId) {
 
     if (cardOneName == cardTwoName) {
       console.log("Yay");
+      // Check if all cards are now opened
+
+      // Check if all cards are now opened
+      const allCards = document.querySelectorAll(".character-div");
+      const allOpened = [...allCards].every((card) =>
+        card.classList.contains("opened")
+      );
+
+      if (allOpened) {
+        setTimeout(function () {
+          window.location.href = "results.html";
+        }, 1000);
+      }
     } else {
       console.log("vale");
 
@@ -39,8 +57,11 @@ function checkHowManyClicked(name, specialId) {
         cardTwo.classList.remove("opened");
         cardTwo.classList.add("closed");
 
+        cardOneID = 0;
+        cardTwoID = 0;
+
         clickCount = 0;
-      }, 1000);
+      }, 800);
     }
     clickCount = 0;
     cardOneName = "";
