170dfb605276ddd9dd5bb15c1d2bae830a0baa41
Hanna Kass <hanna.kass7@gmail.com>
2025-12-16 10:11:12 +0200

    filter out characters who don't have a picture

--- js/script.js
+++ js/script.js
@@ -8,34 +8,38 @@ function gameboard() {
       return response.json();
     })
     .then(function (response) {
+      /* filter out characters who don't have a picture */
+      const validCharacters = response.data.filter(
+        (character) => character.imageUrl
+      );
+
       for (let i = 0; i < 8; i++) {
-        const randomNumber = Math.floor(Math.random() * response.data.length);
+        const randomNumber = Math.floor(Math.random() * validCharacters.length);
 
         /* creating new HTML elements */
         for (let j = 0; j < 2; j++) {
           const newDiv = document.createElement("div");
           const image = document.createElement("img");
-          //const name = document.createElement("p");
+          const name = document.createElement("p");
 
+          /* adding classes */
           image.classList.add("character-image");
-          newDiv.classList.add(i);
-          console.log(newDiv);
-          //name.classList.add("character-name");
+          name.classList.add("character-name");
 
           /* applying API information to the elements I created*/
           image.src = response.data[randomNumber].imageUrl;
-          //name.textContent = response.data[randomNumber].name;
+          name.textContent = response.data[randomNumber].name;
 
           /* putting the information into the HTML */
           newDiv.appendChild(image);
-          //newDiv.appendChild(name);
+          newDiv.appendChild(name);
           memoboard.appendChild(newDiv);
         }
       }
     });
 
   //have to delay it because pictures display slower
-  setTimeout(shuffleBoard, 400);
+  setTimeout(shuffleBoard, 600);
 }
 
 function shuffleBoard() {
